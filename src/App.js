d99dd24d648b868f33a9bd606aa17cd178fcdc27
DESKTOP-5KBKNRR\maarek <maareks1994@gmail.com>
2021-05-01 20:06:30 +0300

    working restaraunts tests with api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 const App = () => (
-  <div>
+    <Provider store={store}>
     <RestaurantScreen />
-  </div>
+    </Provider>
 );
 
 export default App;
